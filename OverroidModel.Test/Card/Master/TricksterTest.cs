8a4bdea0bc5d5f13b9769c2d1b4b74ca264f6d97
datakdtk <0k.datak.ya@gmail.com>
2021-01-24 17:53:59 +0900

    Create TricksterTest

---
+++ OverroidModel.Test/Card/Master/TricksterTest.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using OverroidModel.Card;
+using OverroidModel.Game.Actions;
+using OverroidModel.Test.TestLib;
+using Xunit;
+
+namespace OverroidModel.Test.Card.Master
+{
+    public class TricksterTest
+    {
+
+        [Fact]
+        public void Test_LosesToInnocence()
+        {
+            var game = TestGameBuilder.CreateIndividualGame(
+                round: 1,
+                cardNamesInOverroidHand: new List<CardName>() { CardName.Trickster }, // Attacking
+                cardNamesInHumanHand: new List<CardName>() { CardName.Innocence } // Defending
+                );
+
+            TestGameBuilder.SetCardsToCurrentBattle(CardName.Trickster, CardName.Innocence, game);
+
+            CustomAssertion.LosesInLastRound(CardName.Trickster, game);
+            CustomAssertion.ActionIsInHistory<ReversalEffect>(game.ActionHistory);
+        }
+
+        [Fact]
+        public void Test_WinsToDiva()
+        {
+            var game = TestGameBuilder.CreateIndividualGame(
+                round: 1,
+                cardNamesInOverroidHand: new List<CardName>() { CardName.Trickster }, // Attacking
+                cardNamesInHumanHand: new List<CardName>() { CardName.Diva } // Defending
+                );
+
+            TestGameBuilder.SetCardsToCurrentBattle(CardName.Trickster, CardName.Diva, game);
+
+            CustomAssertion.WinsInLastRound(CardName.Trickster, game);
+            CustomAssertion.ActionIsInHistory<ReversalEffect>(game.ActionHistory);
+        }
+
+        [Fact]
+        public void Test_WinsToOverroid()
+        {
+            var game = TestGameBuilder.CreateIndividualGame(
+                round: 1,
+                cardNamesInOverroidHand: new List<CardName>() { CardName.Trickster }, // Attacking
+                cardNamesInHumanHand: new List<CardName>() { CardName.Overroid } // Defending
+                );
+
+            TestGameBuilder.SetCardsToCurrentBattle(CardName.Trickster, CardName.Overroid, game);
+
+            CustomAssertion.WinsInLastRound(CardName.Trickster, game);
+            CustomAssertion.ActionIsInHistory<ReversalEffect>(game.ActionHistory);
+            CustomAssertion.ActionIsNotInHistory<SingularityEffect>(game.ActionHistory);
+        }
+    }
+}
