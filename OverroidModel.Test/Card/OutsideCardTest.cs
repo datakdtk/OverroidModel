442623007f280aeaec16368123e67810947b3247
datakdtk <0k.datak.ya@gmail.com>
2021-01-26 18:27:30 +0900

    Create OutsideCard

---
+++ OverroidModel.Test/Card/OutsideCardTest.cs
@@ -0,0 +1,65 @@
+﻿using OverroidModel.Card;
+using OverroidModel.Card.Master;
+using OverroidModel.Exceptions;
+using Xunit;
+
+namespace OverroidModel.Test.Card
+{
+    public class OutsideCardTest
+    {
+        [Fact]
+        public void Test_IsViewable_ToOnePlayer()
+        {
+            var card = new OutsideCard(new Idol());
+            var p1 = new PlayerAccount("hoge");
+            var p2 = new PlayerAccount("fuga");
+
+            Assert.False(card.IsViewableTo(p1));
+            Assert.False(card.IsViewableTo(p2));
+
+            card.RevealTo(p1);
+            
+            Assert.True(card.IsViewableTo(p1));
+            Assert.False(card.IsViewableTo(p2));
+        }
+
+        [Fact]
+        public void Test_IsViewable_ToTwoPlaers()
+        {
+            var card = new OutsideCard(new Idol());
+            var p1 = new PlayerAccount("hoge");
+            var p2 = new PlayerAccount("fuga");
+
+            Assert.False(card.IsViewableTo(p1));
+            Assert.False(card.IsViewableTo(p2));
+
+            card.RevealTo(p1);
+            card.RevealTo(p2);
+            
+            Assert.True(card.IsViewableTo(p1));
+            Assert.True(card.IsViewableTo(p2));
+        }
+
+        [Fact]
+        public void Test_LookAtBy_WhenViewable()
+        {
+            var card = new OutsideCard(new Idol());
+            var p = new PlayerAccount("hoge");
+
+            card.RevealTo(p);
+            Assert.True(card.IsViewableTo(p));
+
+            var master = card.LookedAtBy(p);
+            Assert.Equal(CardName.Idol, master.Name);
+        }
+
+        [Fact]
+        public void Test_LookAtBy_WhenNotViewable()
+        {
+            var card = new OutsideCard(new Idol());
+            var p = new PlayerAccount("hoge");
+
+            Assert.Throws<UnavailableActionException>( () => card.LookedAtBy(p) );
+        }
+    }
+}
---
+++ OverroidModel/Card/OutsideCard.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using OverroidModel.Card.Master;
+using OverroidModel.Exceptions;
+
+namespace OverroidModel.Card
+{
+    /// <summary>
+    /// Card that is not used in the game. 
+    /// </summary>
+    class OutsideCard
+    {
+        readonly ICardMaster card;
+        readonly List<PlayerAccount> lookingPlayers;
+
+        /// <param name="card">Data of the card.</param>
+        public OutsideCard(ICardMaster card)
+        {
+            this.card = card;
+            lookingPlayers = new List<PlayerAccount>();
+        }
+
+        /// <summary>
+        /// Check if player can see what the card is.
+        /// </summary>
+        /// <param name="player">Player trying to see the card.</param>
+        /// <returns>Returns true if the player can see the card.</returns>
+        public bool IsViewableTo(PlayerAccount player)
+        {
+            return lookingPlayers.Contains(player);
+        }
+
+        /// <summary>
+        /// Get card data if available by given player
+        /// </summary>
+        /// <param name="player">Player trying to see the card.</param>
+        /// <returns>Card data,</returns>
+        /// <exception cref="UnavaibleActionException">Thrown when the player is not authorized to look at the card.</exception>
+        public ICardMaster LookedAtBy(PlayerAccount player)
+        {
+            if (!IsViewableTo(player))
+            {
+                throw new UnavailableActionException("The card is not viewable to given player");
+            }
+            return card;
+        }
+
+        /// <summary>
+        /// Make given player be able to look at the card
+        /// </summary>
+        /// <param name="player">Player who gets access to the card.</param>
+        internal void RevealTo(PlayerAccount player)
+        {
+            lookingPlayers.Add(player);
+        }
+    }
+}
