a2422f016ea45823dbb4694bdb42c19f8c58108b
datakdtk <0k.datak.ya@gmail.com>
2021-03-04 21:34:20 +0900

    Create general function to create GameCommand

---
+++ OverroidModel.Test/GameAction/Commands/RandomCommandGeneratorTest.cs
@@ -0,0 +1,70 @@
+﻿using OverroidModel.Card;
+using OverroidModel.GameAction.Commands;
+using OverroidModel.Test.TestLib;
+using Xunit;
+
+namespace OverroidModel.Test.GameAction.Commands
+{
+    public class RandomCommandGeneratorTest
+    {
+        [Fact]
+        public void Test_CreateFromRequirement_CardPlacment()
+        {
+            var game = TestGameBuilder.CreateIndividualGame(round: 3);
+            var requirement = new CommandRequirementImplement<CardPlacement>(game.OverroidPlayer);
+            var command = RandomCommandGenerator.CreateFromRequirement(requirement, game);
+            Assert.IsType<CardPlacement>(command);
+        }
+
+        [Fact]
+        public void Test_CreateFromRequirement_Detection()
+        {
+            var game = TestGameBuilder.CreateIndividualGame(round: 3);
+            var requirement = new CommandRequirementImplement<Detection>(game.OverroidPlayer);
+            var command = RandomCommandGenerator.CreateFromRequirement(requirement, game);
+            Assert.IsType<Detection>(command);
+        }
+
+        [Fact]
+        public void Test_CreateFromRequirement_HackCommand()
+        {
+            var game = TestGameBuilder.CreateIndividualGame(round: 3);
+            var requirement = new CommandRequirementImplement<HackCommand>(game.OverroidPlayer);
+            var command = RandomCommandGenerator.CreateFromRequirement(requirement, game);
+            Assert.IsType<HackCommand>(command);
+        }
+
+        [Fact]
+        public void Test_CreateFromRequirement_EspionageCommand()
+        {
+            var game = TestGameBuilder.CreateIndividualGame(round: 3);
+            var requirement = new CommandRequirementImplement<EspionageCommand>(game.OverroidPlayer);
+            var command = RandomCommandGenerator.CreateFromRequirement(requirement, game);
+            Assert.IsType<EspionageCommand>(command);
+        }
+
+        [Fact]
+        public void Test_CreateFromRequirement_MorphCommand()
+        {
+            var game = TestGameBuilder.CreateIndividualGame(round: 1);
+
+            // Go to next round to prepare Morph target
+            TestGameBuilder.SetCardsToCurrentBattle(CardName.Unknown, CardName.Unknown, game);
+            game.ResolveAllActions();
+
+            var requirement = new CommandRequirementImplement<MorphCommand>(game.OverroidPlayer);
+
+            var command = RandomCommandGenerator.CreateFromRequirement(requirement, game);
+            Assert.IsType<MorphCommand>(command);
+        }
+
+        [Fact]
+        public void Test_CreateFromRequirement_RushCommand()
+        {
+            var game = TestGameBuilder.CreateIndividualGame(round: 3);
+            var requirement = new CommandRequirementImplement<RushCommand>(game.OverroidPlayer);
+            var command = RandomCommandGenerator.CreateFromRequirement(requirement, game);
+            Assert.IsType<RushCommand>(command);
+        }
+    }
+}
---
+++ OverroidModel/GameAction/Commands/RandomCommandGenerator.cs
@@ -0,0 +1,16 @@
+﻿namespace OverroidModel.GameAction.Commands
+{
+    public static class RandomCommandGenerator
+    {
+        public static IGameCommand CreateFromRequirement(ICommandRequirement requirement, IGameInformation game) => requirement switch
+        {
+            CommandRequirementImplement<CardPlacement> _ => CardPlacement.CreateRandomCommand(game, requirement.ComandingPlayer),
+            CommandRequirementImplement<Detection> _ => Detection.CreateRandomCommand(game, requirement.ComandingPlayer),
+            CommandRequirementImplement<HackCommand> _ => HackCommand.CreateRandomCommand(game, requirement.ComandingPlayer),
+            CommandRequirementImplement<EspionageCommand> _ => EspionageCommand.CreateRandomCommand(game, requirement.ComandingPlayer),
+            CommandRequirementImplement<MorphCommand> _ => MorphCommand.CreateRandomCommand(game, requirement.ComandingPlayer),
+            CommandRequirementImplement<RushCommand> _ => RushCommand.CreateRandomCommand(game, requirement.ComandingPlayer),
+            _ => throw new System.Exception($"Unexpected type command is expected: {requirement.CommandType}")
+        };
+    }
+}
